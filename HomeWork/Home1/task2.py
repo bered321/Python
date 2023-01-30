4e150f5da0fa0d78c22e569fc4306e8623e4c175
be_red <bered321@inbox.ru>
2023-01-30 23:49:44 +0300

    add discription task2

--- HomeWork/Home1/task2.py
+++ HomeWork/Home1/task2.py
@@ -4,15 +4,16 @@ def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
 cls()
+
 # Задача 2: Найдите сумму цифр трехзначного числа.
 # Примеры:
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
 
 number = int(input('Ведите трёхзначное число: \n'))
-firstNum = number // 100  # первое число
-thirdNum = number % 10   # третье число
-secondNum = (number % 100) // 10  # второе число
+firstNum = number // 100  
+thirdNum = number % 10   
+secondNum = (number % 100) // 10  
 sum = firstNum + secondNum + thirdNum
 print(f'Cумма цифр числа {number} равна: {sum}')
 
