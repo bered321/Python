1264a19b7d23176f46606f035778204bb285e539
be_red <bered321@inbox.ru>
2023-02-18 23:00:18 +0300

    add 2 new solution task 34

--- HomeWork/Home7/Task34/task34.py
+++ HomeWork/Home7/Task34/task34.py
@@ -18,11 +18,39 @@ cls()
 # Ввод:                                       Вывод:
 # пара-ра-рам рам-пам-папам па-ра-па-дам      Парам пам-пам
 
-text = input("Введи текст кричалки: ").lower().split()
-letters = list('аоуыэеёиюя')
-rifma = lambda x: sum (1 for i in x if i in letters)
-first_phrase = rifma(text[0])
-if all([rifma(i) == first_phrase for i in text]):
+# text = input("Введи текст кричалки: ").lower().split()
+# letters = list('аоуыэеёиюя')
+# rifma = lambda x: sum (1 for i in x if i in letters)
+# first_phrase = rifma(text[0])
+# if all([rifma(i) == first_phrase for i in text]):
+#     print('Парам пам-пам')
+# else:
+#     print('Пам парам')
+
+
+def count(a, b):
+    my_list = list()
+    for phraze in song:
+        summa = 0
+        for symbol in phraze:
+            if symbol in vowels:
+                summa += 1
+        my_list.append(summa)
+    return my_list
+
+
+vowels = list('аоуыэеёиюя')
+song = input('введите текст песни винипуха: ').lower().split()
+
+my_list_2 = count(song, vowels)
+if my_list_2.count(my_list_2[0]) == len(my_list_2):
     print('Парам пам-пам')
 else:
     print('Пам парам')
+
+# if len(set(my_list_2)) == 1:
+#      print('Парам пам-пам')
+# else:
+#      print('Пам парам')
+
+
